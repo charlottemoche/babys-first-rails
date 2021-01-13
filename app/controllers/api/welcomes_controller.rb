2679d75cb0265becb436550715a557d249bc174e
charlotte <cmoche123@gmail.com>
2021-01-12 22:03:04 -0500

    first commit

---
+++ app/controllers/api/welcomes_controller.rb
@@ -0,0 +1,11 @@
+class Api::WelcomesController < ApplicationController
+  def hello
+    @random = Random.new.rand(100)
+    render "hello.json.jb"
+  end
+
+  def about
+    @favorite_language = "My favorite language is ruby"
+    render "about.json.jb"
+  end
+end
